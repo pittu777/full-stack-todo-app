6746f159a311b5bc63b6a09644da88b1af4eacd7
pittu777 <pittuprasanth98@gmail.com>
2024-03-17 18:32:43 +0530

    update

--- Server.js
+++ Server.js
@@ -4,6 +4,7 @@ const routes = require("./routes/ToDoRoute");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000; // Corrected the typo here from 'port' to 'PORT'
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 mongoose.connect(process.env.MONGODB_URL).then(() => console.log(`connected`)).catch((err) => console.log(err));
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // Corrected the console log to show the port number
